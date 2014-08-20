b54377b162a8ed34256e2447bbd55438a5da30ab
Jon Stødle <jonstodle@outlook.com>
2014-08-20 20:22:57 +0200

    Add constructors

--- ImdbMovieCollector/Person.cs
+++ ImdbMovieCollector/Person.cs
@@ -3,39 +3,70 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ImdbInterface {
-    public class Person : INotifyPropertyChanged{
+    public class Person : INotifyPropertyChanged {
+        private const string IMDB_BASE_URI = "http://www.imdb.com/title/";
+        private const string URI_NOT_VALID = "Not a valid IMDb uri";
+        private const string ID_NOT_VALID = "Not a valdi IMDb ID";
+        private const string IMDB_ID_REGEX = @"\bnm\d+\b";
         public event PropertyChangedEventHandler PropertyChanged;
 
         #region Constructors
         public Person() { }
 
         public Person(string imdbId) {
-            //TODO: Implement constructor
+            if(imdbId == null) throw new ArgumentNullException(ID_NOT_VALID);
+            ImdbId = imdbId;
         }
 
         public Person(Uri imdbUri) {
-            //TODO: Implement constructor
+            if(imdbUri == null) throw new ArgumentNullException(URI_NOT_VALID);
+            ImdbId = ParseImdbUri(imdbUri);
+        }
+        #endregion
+
+        #region Parsers
+        private string ParseImdbUriString(string imdbUriString) {
+            var match = Regex.Match(imdbUriString, IMDB_ID_REGEX, RegexOptions.IgnoreCase);
+            if(match.Success) return match.Value;
+            else throw new ArgumentException(URI_NOT_VALID);
+        }
+        //TODO: Implement uri parser
+        private string ParseImdbUri(Uri imdbUri) {
+            return ParseImdbUriString(imdbUri.ToString());
+        }
+        #endregion
+
+        private bool CheckValidity(string imdbId) {
+            return Regex.IsMatch(imdbId, IMDB_ID_REGEX, RegexOptions.IgnoreCase);
         }
 
-        #region Init helpers
-        //TODO: Implement validity check
-        //TODO: Implement string parser
-        //TODO: Implement uri parser
-        #endregion
-        #endregion
+        private string _imdbId;
+        public string ImdbId {
+            get { return _imdbId; }
+
+            set {
+                if(value != _imdbId) {
+                    _imdbId = value;
+                    OnPropertyChanged("ImdbId");
+                }
+            }
+        }
 
         private string _name;
         public string Name {
             get { return _name; }
 
             set {
-                if(value != _name) {
-                    _name = value;
-                    OnPropertyChanged("Name");
-                }
+                if(CheckValidity(value)) {
+                    if(value != _name) {
+                        _name = value;
+                        OnPropertyChanged("Name");
+                    }
+                } else throw new ArgumentException(ID_NOT_VALID);
             }
         }
 
@@ -47,18 +78,6 @@ namespace ImdbInterface {
                 if(value != _character) {
                     _character = value;
                     OnPropertyChanged("Character");
-                }
-            }
-        }
-
-        private string _imdbId;
-        public string ImdbId {
-            get { return _imdbId; }
-
-            set {
-                if(value != _imdbId) {
-                    _imdbId = value;
-                    OnPropertyChanged("ImdbId");
                 }
             }
         }
