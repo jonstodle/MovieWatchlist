8b2663c941ac8f54f7df7e8b623b9284236a76ea
Jon Stødle <jonstodle@outlook.com>
2014-09-04 23:01:08 +0200

    Add cast member struct

---
+++ ImdbInterface/CastMember.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImdbInterface {
+    public struct CastMember {
+        private readonly Person person;
+        private readonly string role;
+
+        public CastMember(Person person, string role) {
+            this.person = person;
+            this.role = role;
+        }
+
+        public Person Person {
+            get {
+                return person;
+            }
+        }
+
+        public string Role {
+            get {
+                return role;
+            }
+        }
+
+        public override string ToString() {
+            return string.Format("{0} as {1}", Person, Role);
+        }
+    }
+}
